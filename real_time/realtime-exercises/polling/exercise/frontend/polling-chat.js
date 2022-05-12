0722d767e185a76c537c5259dc4a2ff5763ed75b
taadams1 <rainedoescode@gmail.com>
2022-05-12 18:00:36 -0500

    Progress on real time course

---
+++ real_time/realtime-exercises/polling/exercise/frontend/polling-chat.js
@@ -0,0 +1,74 @@
+const chat = document.getElementById("chat");
+const msgs = document.getElementById("msgs");
+
+// let's store all current messages here
+let allChat = [];
+
+// the interval to poll at in milliseconds
+const INTERVAL = 3000;
+
+// a submit listener on the form in the HTML
+chat.addEventListener("submit", function (e) {
+  e.preventDefault();
+  postNewMsg(chat.elements.user.value, chat.elements.text.value);
+  chat.elements.text.value = "";
+});
+
+async function postNewMsg(user, text) {
+  // post to /poll a new message
+  // write code here
+  const data = {
+    user,
+    text,
+  };
+
+  const options = {
+    method: 'POST',
+    body: JSON.stringify(data),
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  };
+
+  await fetch('/poll', options);
+}
+
+async function getNewMsgs() {
+  // poll the server
+  // write code here
+  let json;
+  try {
+    const res = await fetch('/poll');
+    json = await res.json();
+  } catch (err) {
+    console.error('polling error', err);
+  }
+  allChat = json.msg;
+  render();
+
+  let timeToNextRequest = 0;
+  async function rafTimer(time) {
+    if (timeToNextRequest <= time) {
+      await getNewMsgs();
+      timeToNextRequest = Date.now() + INTERVAL;
+    }
+    requestAnimationFrame(rafTimer);
+  }
+  requestAnimationFrame(rafTimer);
+}
+
+function render() {
+  // as long as allChat is holding all current messages, this will render them
+  // into the ui. yes, it's inefficent. yes, it's fine for this example
+  const html = allChat.map(({ user, text, time, id }) =>
+    template(user, text, time, id)
+  );
+  msgs.innerHTML = html.join("\n");
+}
+
+// given a user and a msg, it returns an HTML string to render to the UI
+const template = (user, msg) =>
+  `<li class="collection-item"><span class="badge">${user}</span>${msg}</li>`;
+
+// make the first request
+getNewMsgs();
